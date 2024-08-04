d1268d829784f6061e38e0adb7ee527e85cd7e5d
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-08-04 06:40:25 +0000

    課題提出

---
+++ 06_task.js
@@ -0,0 +1,16 @@
+let fruit = ['apricot','strawberry'];
+
+console.log(fruit);
+
+
+fruit.unshift('pear');
+
+console.log(fruit);
+
+fruit.push('grape');
+
+console.log(fruit);
+
+fruit[3] = 'lime';
+
+console.log(fruit);
